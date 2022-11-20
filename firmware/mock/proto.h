d9595e2e4502b5dec0ffa3f872bf94a638e582d2
Laurentiu Ciobanu <laurentiu.ciobanu@sessions.us>
2022-11-20 01:36:31 +0000

    sync proto

--- firmware/mock/proto.h
+++ firmware/mock/proto.h
@@ -5,8 +5,20 @@
 #include <stdint.h>
 
 typedef struct {
+} InitBeginFrame;
+#define ID_INIT_BEGIN_FRAME 1
+
+typedef struct {
+    uint8_t type;
+    uint8_t id;
+    uint32_t pos_x;
+    uint32_t pos_y;
+} InitActorFrame;
+#define ID_INIT_ACTOR_FRAME 2
+
+typedef struct {
     uint8_t a;
     uint8_t b;
 } TestPacket;
-#define ID_TEST_PACKET 1
+#define ID_TEST_PACKET 241
 
